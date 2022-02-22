90f6877bf00896ae8e7ecdc54b6cf0bdfe45c212
Vincent moon <88290726+Tin3668@users.noreply.github.com>
2022-02-21 20:05:59 -0800

    added main.cpp and Auto_Clicker.exe

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <Windows.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+cout << "To start the auto-clicking press space to stop press space again to change the start/stop key press c\n";
+bool start = true;
+while (start)
+{
+    if (GetKeyState(' '))
+    {
+        do
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            Sleep(10);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        } while (GetKeyState(' ') && start != false);
+        start = false;
+        break;
+    }
+}
+    return 0;
+}
