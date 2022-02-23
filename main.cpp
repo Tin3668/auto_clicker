0fcb4ef6e6ca67930c077f06f2586de9d485d0f8
Vincent moon <88290726+Tin3668@users.noreply.github.com>
2022-02-22 19:47:51 -0800

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,24 @@ using namespace std;
 
 int main()
 {
-cout << "To start the auto-clicking press space to stop press space again to change the start/stop key press c\n";
+    cout << "To start the auto-clicking press space to stop press space again\n";
 
-bool start = true;
-while (start)
-{
-    if (GetKeyState(' '))
+    bool start = true;
+    bool pause = false;
+
+    while (start)
     {
-        do
+        if (GetKeyState(' '))
         {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-            Sleep(10);
-            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-        } while (GetKeyState(' ') && start != false);
-        start = false;
-        break;
+            do
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                Sleep(10);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            } while (GetKeyState(' '));
+            start = false;
+            break;
+        }
     }
-}
     return 0;
 }
